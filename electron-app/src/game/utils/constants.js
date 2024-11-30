dd63bcf620bbdb725357a07c7cd3f7d4f0bd03b9
ChrisLoui <lmabrau@up.edu.ph>
2024-11-30 22:15:48 +0800

    try

--- electron-app/src/game/utils/constants.js
+++ electron-app/src/game/utils/constants.js
@@ -4,7 +4,7 @@ export const PLAYER_HEIGHT = 64;
 export const PLAYER_WIDTH = 64;
 export const PLAYER_START_X = 330;
 export const PLAYER_START_Y = 100;
-export const PLAYER_SPEED = 2;
+export const PLAYER_SPEED = 0.8;
 
 
 
@@ -18,4 +18,4 @@ export const GHOST_HEIGHT = 64;
 export const GHOST_WIDTH = 64;
 export const GHOST_START_X = 330;
 export const GHOST_START_Y = 100;
-export const GHOST_SPEED = 2.25;
+export const GHOST_SPEED = 1.5;
